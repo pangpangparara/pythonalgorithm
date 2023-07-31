95ad6a79b6295b489a9555ad75b66fc1c517d9fe
pangpangparara <chusan5336@gmail.com>
2023-08-01 07:54:11 +0900

    zonver can do it

--- 02.자료구조/04.stack_backjoon_No9012.py
+++ 02.자료구조/04.stack_backjoon_No9012.py
@@ -1,16 +1,21 @@
 import sys
 
-stack = []
 n = int(input())
 
 for i in range(n):
-    a = sys.stdin.readline().split('(')
-    print(a)
-#    for j in range(len(a)):
-#         print(j)
-#         print(a[j])
-#         if a[j] == '(':
-#             stack.push(a[j])
-#         elif a[j]== ')':
-#             stack.pop(-1)  
-
+    stack = []
+    a = input()
+    for j in a:
+        if j == '(':   
+            stack.append(j)
+        elif j == ')':
+            if len(stack) == 0 :
+                print("NO")
+                break
+            else :  
+                stack.pop(-1) 
+    else:  ## for - else 문 for 문에서 break 만나지 않았을 경우 실행         
+        if len(stack) != 0:
+            print("NO")
+        elif len(stack) == 0:
+            print("YES")
