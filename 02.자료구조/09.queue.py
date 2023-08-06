d1daf0cbd1c177f79cbc68ba02e9ca27173f7bb6
pangpangparara <chusan5336@gmail.com>
2023-08-06 16:53:58 +0900

    Que

--- 02.자료구조/09.queue.py
+++ 02.자료구조/09.queue.py
@@ -1,22 +1,36 @@
+# 클래스를 만들어보자
+'''
+class Person:
+    def greeting(self):
+        print('Hello')
+
+jame = Person()
+jame.greeting()
+'''
+
 # 큐를 만들어보자
 class Queue:
     def __init__(self):
-         self =[]
-         front_index =0
+        self.items =[]
+        self.front_index =0
+        print("큐 선언")
+      
+    def enqueue(self,val):
+        self.items.append(val)
 
-    # def enqueue(self,val):
-    #     self.append(val)
+    def deque(self):
+        if self.front_index == len(self.items):
+            print('Queue is Empty')
+        else:
+            x = self.items[self.front_index]
+            self.front_index +=1
+            return x
 
-    # def deque(self):
-    #     if self.index[0] == len(self):
-    #         print('Queue is Empty')
-    #     else:
-    #         x = self[self.index]
-    #         self.index +=1
-    #         return x
-        
-# __init__(que)
-# enqueue(que,1)
-# enqueue(que,2)
-# enqueue(que,3)
-# deque(que)
+que = Queue()
+que.enqueue(1)
+print(que.deque())
+que.enqueue(2)
+que.enqueue(3)
+print(que.deque())
+print(que.deque())
+print(que.deque())
